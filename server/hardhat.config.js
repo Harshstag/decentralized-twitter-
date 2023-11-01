a1c2e30c01cb2ea8ecec1596d846e8b66c8b713c
Harsh Singh <62803431+Harshstag@users.noreply.github.com>
2023-11-01 17:38:51 +0530

    hardhat.config.js

--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -1,17 +1,3 @@
-// require("@nomicfoundation/hardhat-toolbox");
-// require("dotenv").config();
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.19",
-//   networks: {
-//     mumbai: {
-//       url: "https://polygon-mumbai.g.alchemy.com/v2/XvKLOjFrXzN9yYI3A4O-3Af2rY8dpMLM",
-//       accounts: [
-//         "7eeeb535a6afec56c834dfefa069bce852dfda4e4e72340e36a63d9dcd8bc17f",
-//       ],
-//     },
-//   },
-// };
 
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
@@ -21,16 +7,12 @@ require("dotenv").config();
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
-  // for (const account of accounts) {
-  // console.log(account.address);
 
   const address = await accounts.getAddress();
   console.log(`Contract Address: ${address}`);
   // }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -39,47 +21,10 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/XvKLOjFrXzN9yYI3A4O-3Af2rY8dpMLM",
+      url: "https://polygon-mumbai.g.alchemy.com/v2/xxxxxxxxxxxxxxxxxx",
       accounts: [
-        "7eeeb535a6afec56c834dfefa069bce852dfda4e4e72340e36a63d9dcd8bc17f",
+        "7eeexxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
       ],
     },
   },
 };
-
-// require("@nomiclabs/hardhat-waffle");
-// require("@nomiclabs/hardhat-ethers");
-// const fs = require("fs");
-// // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
-
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
-
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
-
-// module.exports = {
-//   defaultNetwork: "hardhat",
-//   networks: {
-//     hardhat: {
-//       chainId: 1337,
-//     },
-//     mumbai: {
-//       url: "https://polygon-mumbai.g.alchemy.com/v2/XvKLOjFrXzN9yYI3A4O-3Af2rY8dpMLM",
-//       accounts: [
-//         "7eeeb535a6afec56c834dfefa069bce852dfda4e4e72340e36a63d9dcd8bc17f",
-//       ],
-//     },
-//   },
-//   solidity: {
-//     version: "0.8.19",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200,
-//       },
-//     },
-//   },
-// };
